53e531336d7cf7cd45c8c3acd36f6f8ff78e10a5
giovanniytch <giovanni.ytch@gmail.com>
2025-12-07 22:34:50 +0100

    Create variables.py

---
+++ 0/variables.py
@@ -0,0 +1,19 @@
+myvar = 1
+print(myvar)
+
+# this is a comment.
+
+# what is a variable?
+# - it is a container or box, with a label, in which you can store data.
+
+# example: a variable called 'mynum' with the data 20 stored in it.
+mynum = 20
+print(mynum)
+
+# '=' is called the assignment operator.
+# it stores data that is to the right of the assignment operator
+# into the variable that is to the left of the assignment operator.
+
+# exercises:
+# a) declare a variable called 'myname' and store your name in it.
+# b) print the variable called 'myname' to the terminal.
