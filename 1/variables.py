cfc8b25f6ab32e5be3e3c7927b211e0bf5782890
giovanniytch <giovanni.ytch@gmail.com>
2025-12-10 20:39:09 +0100

    Create variables.py

---
+++ 1/variables.py
@@ -0,0 +1,29 @@
+# declare the variables below:
+#    'num_a' with 100 stored in it
+#    'num_b' with 200 stored in it
+#    'res' with 0 stored in it
+num_a = 100
+num_b = 200
+res = 0
+print(res)
+
+# add 'num_a' and 'num_b' and store the result in 'res'
+res = num_a + num_b
+print(res)
+
+# all variables are reassignable in python
+MYCONSTANT = 'python constants'
+print(MYCONSTANT)
+MYCONSTANT = 'are not constant'
+print(MYCONSTANT)
+
+# printing formatted strings
+print(f'{num_a} + {num_b} = {res}')
+
+# exercises:
+# a) add the variables below and print a formatted string:
+#    num_a: -8
+#    num_b: 32
+#    num_c: -64
+#    num_d: 128
+# b) solve exercise a without using the variable 'res'.
